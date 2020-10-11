6f767e6eef9a5412b4fa2bab3dbd824987aa118e
Michał Poręba <michalporeba@gmail.com>
2020-10-11 21:55:48 +0100

    MockLike example with basic dependency injection

---
+++ Experiments/Example.MockLike/Program.cs
@@ -0,0 +1,32 @@
+﻿using UseCases;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.MockLike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for the brevity of the example at the moment I'm not using
+            // DI libraries like Autofac, and instead using bare minimum
+            // based on .net core dependency injection
+            var sp = new ServiceCollection()
+                .AddScoped<IPrinter, Printer>()
+                .AddScoped<IHello, Hello>()
+                .AddScoped<IAdd, Add>()
+                .AddScoped<IThrow, Throw>()
+                .AddScoped<IUseCase<IHello>, UseCase<IHello>>()
+                .AddScoped<IUseCase<IAdd>, UseCase<IAdd>>()
+                .AddScoped<IUseCase<IThrow>, UseCase<IThrow>>()
+                .BuildServiceProvider();
+
+            var greeter = sp.GetService<IUseCase<IHello>>();
+            var adder = sp.GetService<IUseCase<IAdd>>();
+            var thrower = sp.GetService<IUseCase<IThrow>>();
+
+            greeter.Do(x => x.Greet("World"));
+            var result = adder.Do(x => x.Execute(1,2));
+            thrower.Do(x => x.Execute());
+        }
+    }
+}
---
+++ Experiments/Example.MockLike/UseCase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq.Expressions;
+using UseCases;
+
+namespace Example.MockLike
+{
+    public interface IUseCase<T>
+    {
+        void Do(Expression<Action<T>> expression);
+        TResult Do<TResult>(Expression<Func<T, TResult>> expression);
+    }
+    
+    public class UseCase<T> : IUseCase<T>
+    {
+        private readonly T _target;
+        private readonly IPrinter _printer;
+
+        public UseCase(IServiceProvider serviceProvider, IPrinter printer)
+        {
+            _target = (T)serviceProvider.GetService(typeof(T));
+            _printer = printer;
+        }
+        
+        public void Do(Expression<Action<T>> expression)
+        {
+            _printer.Print($"Log: Starting execution of {typeof(T)}");
+            try
+            {
+                expression.Compile().Invoke(_target);
+                _printer.Print($"Log: Finished execution of {typeof(T)}");
+            }
+            catch (Exception ex)
+            {
+                _printer.Print("Log: Failed execution");
+            }
+        }
+
+        public TResult Do<TResult>(Expression<Func<T, TResult>> expression)
+        {
+            _printer.Print($"Log: Starting execution of {typeof(T)}");
+            TResult result;
+            try
+            {
+                result = expression.Compile().Invoke(_target);
+                _printer.Print($"Log: Finished execution of {typeof(T)}");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _printer.Print("Log: Failed execution");
+                throw;
+            }
+        }
+    }
+}
